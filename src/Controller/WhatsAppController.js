39f6dc70766e0f9b1c724c5cef07c32b3a35f187
Bruno Silva <bruno.silva@edi-labs.com>
2020-11-28 18:15:43 -0300

    Eventos do whatsapp

--- src/Controller/WhatsAppController.js
+++ src/Controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -68,6 +69,316 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
         }
+
+        HTMLFormElement.prototype.getForm = function() {
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJson = function() {
+            let json = {};
+            
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value;
+
+            });
+
+            return json;
+        }
+    }
+
+    initEvents(){
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+        
+        });
+
+        this.el.btnNewContact.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if(e.key === 'Enter'){
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault();
+
+            console.log(this.el.formPanelAddContact.toJson());
+
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+            item.on('click', e => {
+
+                this.el.home.hide();
+                this.el.main.css({
+                    display: 'flex'
+                });
+
+            });
+
+        });
+
+        this.el.btnAttach.on('click', e => {
+
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+
+        this.el.btnAttachPhoto.on('click', e => {
+
+            this.el.inputPhoto.click();
+
+        });
+
+        this.el.inputPhoto.on('change', e => {
+
+            console.log(this.el.inputPhoto.files);
+
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
+
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+
+            this.closeAllMainPanel()
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log('Take Picture');
+        });
+
+        this.el.btnAttachDocument.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+            console.log('Send Document');
+        });
+
+        this.el.btnAttachContact.on('click', e => {
+
+            this.el.modalContacts.show();
+
+        });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+
+            this.el.modalContacts.hide();
+
+        });
+
+        this.el.btnSendMicrophone.on('click', e => {
+
+            this.el.recordMicrophone.show();
+            this.startRecordMicrophoneTime();
+            this.el.btnSendMicrophone.hide();
+
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.inputText.on('keyup', e => {
+
+            if(this.el.inputText.innerHTML.length) {
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+
+            } else {
+
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+
+            }
+
+        });
+
+        this.el.inputText.on('keypress', e => {
+
+            if(!e.ctrlKey && e.key === 'Enter'){
+                
+                e.preventDefault();
+                this.el.btnSend.click();
+
+            }
+
+        });
+
+        this.el.btnSend.on('click', e => {
+
+            console.log(this.el.inputText.innerHTML);
+
+        });
+
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e => {
+
+                console.log(emoji.dataset.unicode);
+
+                let img = this.el.imgEmojiDefault.cloneNode();
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name);
+                });
+
+                let cursor = window.getSelection();
+
+                if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
+                    this.el.inputText.focus();
+                    cursor = window.getSelection();
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+
+            });
+
+        });
+
+    }
+
+    startRecordMicrophoneTime(){
+        
+        let start = Date.now();
+        
+        this._recordMicrophoneInterval = setInterval(() => {
+            
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+
+        }, 100);
+    
+    }
+
+    closeRecordMicrophone(){
+
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
+
+    }
+
+    closeAllMainPanel(){
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+    }
+
+    closeMenuAttach(e){
+        document.removeEventListener('click', this.closeMenuAttach)
+        this.el.menuAttach.removeClass('open');
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
     }
 
 }
