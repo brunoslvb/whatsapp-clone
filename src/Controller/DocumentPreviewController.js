58ceca85fb188d5543789dadd65489b300d6ad9c
Bruno Silva <bruno.silva@edi-labs.com>
2020-12-02 14:04:24 -0300

    Lendo a capa do arquivo PDF

--- src/Controller/DocumentPreviewController.js
+++ src/Controller/DocumentPreviewController.js
@@ -1,3 +1,8 @@
+const pdfjsLib = require('pdfjs-dist');
+const path = require('path');
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.resolve(__dirname, '..', '..', 'dist', 'pdf.worker.bundle.js');
+
 export class DocumentPreviewController {
 
     constructor(file) {
@@ -8,6 +13,8 @@ export class DocumentPreviewController {
 
         return new Promise((resolve, reject) => {
 
+            let reader = new FileReader();
+
             console.log(this._file.type);
 
             switch (this._file.type) {
@@ -15,7 +22,7 @@ export class DocumentPreviewController {
                 case 'image/jpeg':
                 case 'image/jpg':
                 case 'image/gif':
-                    let reader = new FileReader();
+
                     reader.onload = e => {
                         resolve({
                             src: reader.result,
@@ -29,7 +36,57 @@ export class DocumentPreviewController {
                 break;
 
                 case 'application/pdf':
-                    
+
+                    console.log('Entrou');
+
+                    reader.onload = e => {
+                        
+                        let pdf = pdfjsLib.getDocument(new Uint8Array(reader.result)).promise.then(pdf => {
+
+                            pdf.getPage(1).then(page => {
+
+                                console.log(page);
+                                
+                                let viewport = page.getViewport({ scale: 1 });
+                                console.log(viewport);
+                                
+                                let canvas = document.createElement('canvas');
+                                let canvasContext = canvas.getContext('2d');
+
+                                canvas.width = viewport.width;
+                                canvas.height = viewport.height;
+
+                                page.render({
+                                    canvasContext,
+                                    viewport
+                                }).promise.then(() => {
+                                    
+                                    let _s = (pdf.numPages > 1) ? 's' : '';
+
+                                    resolve({
+                                        src: canvas.toDataURL('image/png'),
+                                        info: `${pdf.numPages} página${_s}`
+                                    });
+
+                                }).catch(err => {
+                                    console.error(err);
+                                    reject(err);
+                                });
+
+                            }).catch(err => {
+                                reject(err)
+                            })
+
+                        }).catch(err => {
+
+                            reject(err);
+
+                        });
+
+                    }
+
+                    reader.readAsArrayBuffer(this._file);
+
                 break;
 
                 default:
--- src/Controller/WhatsAppController.js
+++ src/Controller/WhatsAppController.js
@@ -254,20 +254,32 @@ export class WhatsAppController {
 
             if(this.el.inputDocument.files.length){
 
+                this.el.panelDocumentPreview.css({
+                    'height': '1%'
+                });
+
                 let file = this.el.inputDocument.files[0];
             
                 this._documentPreviewController = new DocumentPreviewController(file);
 
                 this._documentPreviewController.getPreviewData().then(result => {
 
+                    console.log(result);
+
                     this.el.imgPanelDocumentPreview.src = result.src;
                     this.el.infoPanelDocumentPreview.innerHTML = result.info;
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
+
                 }).catch(err => {
 
-                    console.log(file);
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
 
                     switch (file.type) {
                         case 'application/vnd.ms-excel':
